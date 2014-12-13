de12ef1e677e2caf85fd6605bd1cead693424e54
João Luis Failde <johnnyfailde@gmail.com>
2014-12-13 19:28:54 -0200

    Outra Linha
    Mais uma linha adicionada

--- TesteGit/src/OlaGit.java
+++ TesteGit/src/OlaGit.java
@@ -3,5 +3,6 @@ public class OlaGit {
 	public static void main(String[] args) {
 		System.out.println("Testando o GitHub no Eclipse");
 		System.out.println("Nova linha.");
+		System.out.println("Outra linha.");
 	}
 }
